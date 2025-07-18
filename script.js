28de7db9991760b22b4dad18c5a0a349f6b6fa73
teste <gustavobonjardim06@gmail.com>
2025-07-18 15:04:49 -0300

    commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Isenha = document.querySelector(".senha")
 const Itel = document.querySelector(".tel")
 
 function cadastrar() {
-    fetch("http://localhost:8080/cadastrar", 
+    fetch("http://localhost:8080/cadastrados",
         {
             headers: {
                 'Accept': 'application/json',
@@ -13,16 +13,17 @@ function cadastrar() {
             },
             method: "POST",
             body: JSON.stringify({
-                nome : Inome.value,
+                name : Inome.value,
                 email: Iemail.value,
-                senha: Isenha.value,
-                telefone: Itel.value
+                password: Isenha.value,
+                telephone: Itel.value
 
             })
-            .then(function(res){console.log(res)})
-            .catch(function(res){console.log(res)})
+            
         }
     )
+    .then(function(res){console.log(res)})
+    .catch(function(res){console.log(res)})
 
 
 };
